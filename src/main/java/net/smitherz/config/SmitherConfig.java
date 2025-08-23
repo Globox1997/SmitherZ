2fcb38af9e7dc96c29b736f7b1683d489ac755ab
Globox1997 <mitschke11@web.de>
2025-08-23 12:08:06 +0200

    removed useless config options

--- src/main/java/net/smitherz/config/SmitherConfig.java
+++ src/main/java/net/smitherz/config/SmitherConfig.java
@@ -11,16 +11,14 @@ public class SmitherConfig implements ConfigData {
     public int maxGemSlots = 6;
     public boolean showZeroSlotsInfo = false;
     public boolean showHoldShiftInfo = true;
-    @Comment("If non link chance is set via datapack, default 5% chance")
-    public float defaultLinkChance = 0.05f;
-    @Comment("Always used for unlinking")
-    public float defaultUnlinkChance = 0.2f;
-    @Comment("20% default bonus")
-    public float hammerExtraChance = 0.2f;
+
+    @Comment("Used for items tagged with bonus_items")
+    public float bonusItemExtraChance = 0.2f;
     @Comment("Use if gems provided by global datapack")
     public boolean defaultGems = true;
     public boolean canLinkSameGem = false;
     @Comment("Turn it off if you set drops via datapacks")
     public boolean mobsCanDropGems = true;
+    @Comment("General chance to break the whole item")
     public float linkBreakChance = 0.0001f;
 }
